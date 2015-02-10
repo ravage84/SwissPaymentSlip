0f5ed89d90bb2f95e5f8f524ebf0398aab9c3992
Marc Würth <ravage@bluewin.ch>
2015-02-10 17:12:37 +0100

    DocBlocks improved

--- tests/PaymentSlipDataTest.php
+++ tests/PaymentSlipDataTest.php
@@ -29,8 +29,7 @@ class PaymentSlipDataTest extends \PHPUnit_Framework_TestCase
     protected $slipData;
 
     /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
+     * Setup the object under test
      *
      * @return void
      */
