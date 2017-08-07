e04384ea1865ecdfb46964b5acebc4aa5764701f
Marc Würth <ravage@bluewin.ch>
2017-08-07 20:09:08 +0200

    Fix "A non-numeric value encountered" error

--- src/PaymentSlipData.php
+++ src/PaymentSlipData.php
@@ -892,11 +892,9 @@ abstract class PaymentSlipData
      */
     protected function modulo10($number)
     {
-        $number = intval($number);
-
         $next = 0;
         for ($i=0; $i < strlen($number); $i++) {
-            $next = $this->moduloTable[($next + substr($number, $i, 1)) % 10];
+            $next = $this->moduloTable[($next + intval(substr($number, $i, 1))) % 10];
         }
 
         return (10 - $next) % 10;
